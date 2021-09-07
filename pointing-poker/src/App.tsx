c45979fd1925edd1a9b3235df0a7dcad33d0860e
MielomankA <mielomanka@mail.ru>
2021-09-07 03:06:21 +0300

    TT-greeting-page: add normalize.css

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/header/Header';
-import Greeting from './components/pages/greeting/Greeting';
-import Footer from './components/footer/Footer';
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Header />
-      <Greeting />
-      <Footer />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
