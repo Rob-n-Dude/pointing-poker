ad5f00f8d5ea7da0a326ed8da2d5ce597260d002
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-09-08 02:24:33 +0300

    TT-Lobby-PopUps: create content for CreateIssue popUp

---
+++ pointing-poker/src/components/popup/createIssue/CreateIssue.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import ButtonStyles from '../../../shared/ButtonStyles';
+import Button from '../../button/Button';
+import './createIssue.scss';
+
+const CreateIssue: FC = () => {
+  return (
+    <form className="create-issue">
+      <h2 className="create-issue_title">Creact Issue</h2>
+      <div className="create-issue-item">
+        <p>Title</p>
+        <input type="text" className="input input_with_border" />
+      </div>
+      <div className="create-issue-item">
+        <p>Link</p>
+        <input type="text" className="input input_with_border" />
+      </div>
+      <div className="create-issue-item">
+        <p>Priority</p>
+        <select>
+          <option value="Low">Low</option>
+          <option value="Middle">Middle</option>
+          <option value="Hight">Hight</option>
+        </select>
+      </div>
+      <div className="create-issue_controls">
+        <Button textContent="Yes" action={() => {}} styles={ButtonStyles.default} />
+        <Button textContent="No" action={() => {}} styles={ButtonStyles.white} />
+      </div>
+    </form>
+  );
+};
+
+export default CreateIssue;
