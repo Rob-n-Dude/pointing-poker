e94eb5c9464c1b867384e5a8e4435a1b8bbb8325
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-09-08 23:39:25 +0300

    TT-Lobby-PopUps: clean app.tsx

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import CreateIssue from './components/popup/createIssue/CreateIssue';
-// import KickPlayer from './components/popup/kickPlayer/KickPlayer';
-import PopUp from './components/popup/PopUp';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <PopUp negativeAction={() => {}} positiveAction={() => {}} content={CreateIssue} />
-      {/* <PopUp content={<KickPlayer user={{ name: 'Jon Sina' }} />} /> */}
+
       <Footer />
     </>
   );
