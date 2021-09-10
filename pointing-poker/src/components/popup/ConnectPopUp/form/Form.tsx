7c0c4d2dedcfea236d1baf9db4ea2243780b0c05
MielomankA <mielomanka@mail.ru>
2021-09-11 00:03:18 +0300

    TT-connectPopup-validation: add validation and initials avatar

--- pointing-poker/src/components/popup/ConnectPopUp/form/Form.tsx
+++ pointing-poker/src/components/popup/ConnectPopUp/form/Form.tsx
@@ -1,36 +1,62 @@
-import React, { FC, ChangeEvent, useState } from 'react';
+import React, { FC, useState, useCallback, useEffect } from 'react';
 import ButtonStyles from '../../../../shared/ButtonStyles';
 import Button from '../../../button/Button';
 import './Form.scss';
 
 const Form: FC = () => {
   const [firstName, setFirstName] = useState<string>('');
+  const [isValid, setIsValid] = useState<boolean>(true);
+  const [lastName, setLastName] = useState<string>('');
+  const [initials, setInitials] = useState<string>('');
 
   const validate = /^[A-Za-zа-яА-ЯёЁ]+$/;
 
-  const firstNameValidation = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    if (validate.test(value)) {
-      setFirstName(value);
+  const firstNameValidation = (): boolean => {
+    if (validate.test(firstName)) {
+      return true;
     }
-    console.log(event.target.value);
+    return false;
   };
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (firstNameValidation()) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }
+
+  const setAvatarInitials = useCallback(() => {
+    if (firstName) {
+      if (lastName) {
+        setInitials(firstName.slice(0, 1).toUpperCase() + lastName.slice(0, 1).toUpperCase());
+      } else {
+        setInitials(firstName.slice(0, 1).toUpperCase());
+      }
+    } else {
+      setInitials('');
+    }
+  }, [firstName, lastName]);
+
+  useEffect(() => {
+    setAvatarInitials();
+  }, [firstName, lastName, setAvatarInitials]);
 
   return (
     <div className="form-wrap">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="firstName">
           Your first name:
           <label className="item" htmlFor="firstName">
             <input
               className="input input_with_border longer"
               type="text"
-              name="firstName"
+              name={firstName}
               id="firstName"
-              value={firstName}
-              onChange={firstNameValidation}
+              onChange={(event) => setFirstName(event.target.value)}
             />
-            <div className="error">* Please enter your name</div>
+            {isValid ? null : <div className="error">* Please enter your name (only Latin and Cyrillic)</div>}
           </label>
         </div>
         <div className="lastName">
@@ -41,20 +67,14 @@ const Form: FC = () => {
               type="text"
               name="lastName"
               id="lastName"
-              /* value={lastName} */
+              onChange={(event) => setLastName(event.target.value)}
             />
           </label>
         </div>
         <div className="jobPosition">
           Your job position:
           <label className="item" htmlFor="jobPos">
-            <input
-              className="input input_with_border longer"
-              type="text"
-              name="jobPos"
-              id="jobPos"
-              /*   value={jobPos} */
-            />
+            <input className="input input_with_border longer" type="text" name="jobPos" id="jobPos" />
           </label>
         </div>
         <div className="image">
@@ -67,12 +87,17 @@ const Form: FC = () => {
                 name="imageUser"
                 id="imageUser"
                 accept=".jpg, .jpeg, .png"
-                /*  value={lastName} */
               />
               <Button textContent="Button" action={() => {}} styles={ButtonStyles.default} />
             </label>
           </div>
-          <div className="userPic">RG</div>
+          <div className="userPic">{initials}</div>
+        </div>
+        <div className="connectButtons">
+          <label htmlFor="submit">
+            <input id="submit" type="submit" className="input-submit default" value="Confirm" />
+          </label>
+          <Button textContent="Cancel" action={() => {}} styles={ButtonStyles.white} />
         </div>
       </form>
     </div>
