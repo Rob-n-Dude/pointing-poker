4a966694fe38a2ae262849ec42fc6e512d71ea7b
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-09-11 02:34:49 +0300

    TT-timer-visual: create timer

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Timer from './components/timer/Timer';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Timer secondsTotal={15} />
       <Footer />
     </>
   );
---
+++ pointing-poker/src/components/timer/Timer.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState, FC } from 'react';
+import './timer.scss';
+
+type TTimer = {
+  secondsTotal: number;
+};
+
+const Timer: FC<TTimer> = ({ secondsTotal }) => {
+  const [timeLeft, setTimeLeft] = useState<number>(secondsTotal);
+  const [parsedTime, setParsedTime] = useState<string[]>(['00', '00']);
+  const animationStyle = `countdown ${secondsTotal}s linear forwards`;
+
+  const parseSeconds = useCallback(() => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    let minsToShow: string;
+    let secToShow: string;
+    if (minutes < 10) {
+      minsToShow = `0${minutes}`;
+    } else {
+      minsToShow = `${minutes}`;
+    }
+    if (seconds < 10) {
+      secToShow = `0${seconds}`;
+    } else {
+      secToShow = `${seconds}`;
+    }
+
+    setParsedTime(() => [minsToShow, secToShow]);
+  }, [timeLeft]);
+
+  useEffect(() => {
+    if (timeLeft >= 0) {
+      parseSeconds();
+      setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    }
+  }, [parseSeconds, timeLeft]);
+
+  return (
+    <div id="countdown">
+      <div id="countdown-number">{parsedTime.join(':')}</div>
+      <svg className="countdown-svg">
+        <circle
+          id="circle"
+          r="50"
+          cx="54"
+          cy="54"
+          style={{ animation: animationStyle }}
+          className="countdown-svg-circle"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Timer;
