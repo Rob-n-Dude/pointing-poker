3b96f0397f139b83ff0611461a3fad04094cb6d5
MielomankA <mielomanka@mail.ru>
2021-09-11 02:07:53 +0300

    TT-chat-layout: remove Header margin-bottom

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Chat from './components/chat/Chat';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Chat />
       <Footer />
     </>
   );
---
+++ pointing-poker/src/components/chat/Chat.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import './Chat.scss';
+
+const Chat: FC = () => {
+  return (
+    <div className="chat-wrap">
+      <div className="chat-head">Chat Room</div>
+      <div className="chat">hh</div>
+    </div>
+  );
+};
+
+export default Chat;
