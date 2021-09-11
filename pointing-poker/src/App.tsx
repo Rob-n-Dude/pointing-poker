af719611080e8329c1ed06808834b779bd1e1f82
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-09-11 22:55:17 +0300

    TT-timer-visual: clean app.tsx

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Timer from './components/timer/Timer';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <Timer secondsTotal={500} isStarted />
       <Footer />
     </>
   );
