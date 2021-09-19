2c75c4e69969192e50fde56eb69d517ae2badc60
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-09-19 21:35:16 +0300

    TT-game-page: clear app.tsx

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import GamePage from './components/pages/game/GamePage';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <GamePage />
       <Footer />
     </>
   );
