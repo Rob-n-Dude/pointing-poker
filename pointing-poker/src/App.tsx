b6e1b7aef8945decfe3b500c59842c40065a8a75
MielomankA <mielomanka@mail.ru>
2021-09-24 00:19:46 +0300

    TT-page-result: add basic layout results-page

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Score from './components/pages/score/Score';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <Score />
       <Footer />
     </>
   );
--- pointing-poker/src/components/pages/score/Score.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import './Score.scss';
-
-const Score: FC = () => {
-  return <></>;
-};
-
-export default Score;
