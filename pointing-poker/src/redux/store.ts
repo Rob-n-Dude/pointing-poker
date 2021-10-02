e7bb7cf2609e33764f05604ff03eb493a7e18128
MielomankA <79061120+MielomankA@users.noreply.github.com>
2021-10-03 00:24:48 +0300

    Update store.ts

--- pointing-poker/src/redux/store.ts
+++ pointing-poker/src/redux/store.ts
@@ -17,6 +17,7 @@ const initialState: TStore = {
 
 const middleware = [thunk];
 
+// add reducer to combineReducers
 const rootReducer = combineReducers({ user: userReducer });
 
 const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
